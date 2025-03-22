913df87b61e022468f5655cbccb8a520d23fc841
Liliam Santos <liliam.santos@pditabira.com>
2025-03-22 20:18:47 -0300

    ok

---
+++ modulo2/aula2_questao4.py
@@ -0,0 +1,1 @@
+#Uma conta poupança foi aberta com um depósito de R$500,00. Esta conta é remunerada em 1% de juros ao mês. O código a seguir apresenta uma forma de implementação para calcular três meses de acúmulo de juros. Reescreva esse código usando apenas duas variáveis.
