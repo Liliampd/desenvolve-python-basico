12c459024de6603af90335d9d295ab1889a3e79c
Liliam Santos <liliam.santos@pditabira.com>
2025-03-23 21:12:00 -0300

    ok

---
+++ modulo2/aula4_questao3.py
@@ -0,0 +1,5 @@
+'''Você está desenvolvendo um programa para calcular o preço total de uma compra em uma loja online. O preço dos produtos é calculado multiplicando a quantidade pelo preço unitário. Escreva um programa em Python que solicite do usuário o nome, o preço unitário e a quantidade de 3 produtos comprados e imprima ao final o preço total da compra. Note no exemplo a seguir que:
+Cada entrada de dados tem uma mensagem indicando o dado solicitado (mensagens em itálico, dado de entrada em negrito)
+A saída deve estar formatada exatamente como indicado (a string "Total: R$" e o preço com um separador de milhar e duas casas decimais).
+'''
+
