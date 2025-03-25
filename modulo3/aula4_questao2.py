0410e70e0c5de6443164108080e9b8421e8962dc
Liliam Santos <liliam.santos@pditabira.com>
2025-03-25 11:06:13 -0300

    ok

--- modulo3/aula4_questao2.py
+++ modulo3/aula4_questao2.py
@@ -11,13 +11,13 @@ avaliacao = int(input("Insira a avaliação do filme (de 1 a 5): "))
 # Verifica a avaliação e imprime a mensagem correspondente
 if avaliacao == 5:
     print("Excelente!")
-elif avaliacao == 4:
+if avaliacao == 4:
     print("Muito Bom!")
-elif avaliacao == 3:
+if avaliacao == 3:
     print("Bom!")
-elif avaliacao == 2:
+if avaliacao == 2:
     print("Regular.")
-elif avaliacao == 1:
+if avaliacao == 1:
     print("Ruim.")
 else:
     print("Avaliação inválida. Insira um número de 1 a 5.")
